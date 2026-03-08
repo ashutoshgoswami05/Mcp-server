002503b185f05986933f492380c43c3173b7f608
Ashutosh Goswami <ashutoshgoswami05@gmail.com>
2026-03-08 15:20:18 +0100

    remote DB config added

--- query_exec.py
+++ query_exec.py
@@ -1,15 +1,29 @@
 
 import sqlite3
+import os
+import libsql_client
+from dotenv import load_dotenv
+
+# Load variables from .env
+load_dotenv()
+
+URL = os.getenv("DB_URL")
+TOKEN = os.getenv("DB_TOKEN")
+
+
 
 def execute_db_query(query,params):
-    conn = sqlite3.connect('company_vitals.db')
-    cursor = conn.cursor()
-    cursor.execute(query,params)
-    result=cursor.fetchone()[0]
-
-    print(result)
-
-    conn.close()
-
-    return result
+    with libsql_client.create_client_sync(url=URL, auth_token=TOKEN) as client:
+            # 3. Execute the query
+            # Turso's execute handles both the query and params
+            result_set = client.execute(query, params or [])
+            
+            # 4. Handle the result
+            # .rows returns a list of rows; we check if it's not empty
+            if result_set.rows:
+                result = result_set.rows[0][0]
+                print(result)
+                return result
+            
+            return None
     
