1a062b8ef6722c63bdad00c7a11d527590912417
Ashutosh Goswami <ashutoshgoswami05@gmail.com>
2026-03-08 21:53:53 +0100

    typo fixes

--- query_exec.py
+++ query_exec.py
@@ -1,28 +1,20 @@
 
 import sqlite3
 import os
-import libsql_client
+import libsql
 from dotenv import load_dotenv
 
-# Load variables from .env
 load_dotenv()
 
-URL = os.getenv("DB_URL")
-TOKEN = os.getenv("DB_TOKEN")
-
-
-
 def execute_db_query(query,params):
-    with libsql_client.create_client_sync(url=URL, auth_token=TOKEN) as client:
-            # 3. Execute the query
-            # Turso's execute handles both the query and params
-            result_set = client.execute(query, params or [])
-            
-            # 4. Handle the result
-            # .rows returns a list of rows; we check if it's not empty
-            if result_set.rows:
-                result = result_set.rows[0][0]
-                print(result)
-                return result  
-    return None
+    try:
+        url = os.getenv("DB_URL")
+        auth_token = os.getenv("DB_TOKEN")
+        print("trying connection",auth_token,url)
+        conn = libsql.connect("imported.db", sync_url=url, auth_token=auth_token)
+        result=conn.execute(query,params).fetchall()
+        conn.commit()
+        return result
+    except Exception as e:
+         print("Exception occured ",e)
     
